4658fd9a8abcb83fe88bb456b5a5b395866e155c
vasavisandhya <eegalapati_vasavisandhya@columbusstate.edu>
2023-04-27 15:25:19 +0000

    project

--- Nutrition/Model/NutritionDetails.cs
+++ Nutrition/Model/NutritionDetails.cs
@@ -11,6 +11,49 @@ namespace Nutrition.Model
 
         [JsonPropertyName("totalWeight")]
         public double totalWeight { get; set; }
+
+        [JsonPropertyName("totalNutrients")]
+        public totalNutrients thetotalNutrients { get; set; }
+    }
+
+    public class totalNutrients
+    {
+        [JsonPropertyName("FAT")]
+        public fat theFat { get; set; }
+
+        [JsonPropertyName("PROCNT")]
+        public procnt theProcnt { get; set; }
+
+        [JsonPropertyName("CHOCDF")]
+        public chocdf theChocdf { get; set; }
+
+        [JsonPropertyName("FE")]
+        public fe theFe { get; set; }
+
+    }
+
+    public class fat
+    {
+        [JsonPropertyName("quantity")]
+        public double theFatQuan { get; set; }
+    }
+
+    public class procnt
+    {
+        [JsonPropertyName("quantity")]
+        public double theProQuan { get; set; }
+    }
+
+    public class chocdf
+    {
+        [JsonPropertyName("quantity")]
+        public double theChocdfQuan { get; set; }
+    }
+
+    public class fe
+    {
+        [JsonPropertyName("quantity")]
+        public double theFeQuan { get; set; }
     }
 
 }
